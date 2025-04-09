8b7e58e1fa768ec6c541380526be465a2685c482
ShockerAttack01 <shockerattack01@outlook.com>
2025-04-09 13:05:32 -0500

    Revert to base

--- src/main/java/shocker/mod/learningmod/Learningmod.java
+++ src/main/java/shocker/mod/learningmod/Learningmod.java
@@ -1,14 +1,11 @@
 package shocker.mod.learningmod;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.Registry;
 
 public class Learningmod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        SCREEN_HANDLER_TYPE = Registry.register(Registries.SCREEN_HANDLER, ExampleBlock.ID,
-                new ScreenHandlerType<>((syncId, inventory) -> new ExampleGuiDescription(syncId, inventory, ScreenHandlerContext.EMPTY),
-                        FeatureFlags.VANILLA_FEATURES));
+
     }
 }
