89af8ab6c23f6a7ee5ca2a19f026af14ea2d9319
ShockerAttack01 <shockerattack01@outlook.com>
2025-04-27 19:06:08 -0500

    Update Code To Functional

---
+++ src/client/java/shocker/learningmod/LearningModClient.java
@@ -0,0 +1,10 @@
+package shocker.learningmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
